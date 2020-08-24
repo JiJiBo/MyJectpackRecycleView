9d099c38a2c40cccfee7dd5e7ba66b8f58626000
xieangxuan <767950460@qq.com>
2020-08-24 09:47:58 +0800

    优化

--- app/src/main/java/com/rulerbug/myjectpackdemo/JockDataSourceFactory.java
+++ app/src/main/java/com/rulerbug/myjectpackdemo/JockDataSourceFactory.java
@@ -4,13 +4,13 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.paging.DataSource;
 
 public class JockDataSourceFactory extends DataSource.Factory<Integer, JockBean.ResultBean.DataBean> {
-    private MutableLiveData<JockDataSourse> list =new MutableLiveData<>();
+//    private MutableLiveData<JockDataSourse> list =new MutableLiveData<>();
 
 
     @Override
     public DataSource<Integer, JockBean.ResultBean.DataBean> create() {
         JockDataSourse dataSource = new JockDataSourse();
-        list.postValue(dataSource);
+//        list.postValue(dataSource);
         return dataSource;
     }
 }
